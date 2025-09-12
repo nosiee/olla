67a653e9116c9ed012edcbd829c3f4d4294a5265
nosiee <lusto.denis@wildberries.work>
2025-09-12 18:47:44 +0300

    feat: quick fixes

--- tunnels/src/header.rs
+++ tunnels/src/header.rs
@@ -3,7 +3,6 @@ use std::net::SocketAddr;
 use std::net::{IpAddr, Ipv4Addr};
 
 pub const HEADER_SIZE: usize = 0x10;
-pub const MAX_IP_FRAME_SIZE: usize = 0x400;
 
 #[derive(Debug, Clone)]
 pub struct HeaderFrame {
@@ -36,10 +35,6 @@ pub fn decode(buf: [u8; HEADER_SIZE]) -> HeaderFrame {
 }
 
 fn total_packet_size(payload: &mut [u8]) {
-    if payload.len() >= MAX_IP_FRAME_SIZE {
-        panic!("unexpected: the ip frame size {} >= {}", payload.len(), MAX_IP_FRAME_SIZE)
-    }
-
     let len = u32::try_from(payload.len()).unwrap();
     payload[0..4].copy_from_slice(&len.to_be_bytes());
 }
